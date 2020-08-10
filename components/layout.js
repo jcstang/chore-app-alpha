a6ce7c37b9e438862f3de9b975dac877b5650188
Jacob Stanger <jacobcstanger@gmail.com>
2020-08-10 13:34:20 -0600

    edit css

--- components/layout.js
+++ components/layout.js
@@ -4,47 +4,39 @@ import styles from '../styles/Home.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faHome, faList, faUser } from '@fortawesome/free-solid-svg-icons'
 
+
 export default function Layout({
   children,
   title = 'This is the default title',
 }) {
   return (
-    <div>
+    <div className="contianer-fluid">
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <header>
-        <nav>
-          <Link href="/">
-            <a>Home</a>
-          </Link>{' '}
-          |
-          <Link href="/about">
-            <a>About</a>
-          </Link>{' '}
-          |
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </nav>
+      <header className="main-layout">
+        <div className="row">
+          <div className="col-10"></div>
+          <div className="col-2 center">
+            <FontAwesomeIcon icon={faUser} size="3x" />
+          </div>
+        </div>
       </header>
 
       {children}
 
-      <footer className={styles.card}>
-        <div className="container">
-          <div className="row">
-            <div className="col text-center"><FontAwesomeIcon icon={faList} size="3x" /></div>
-            <div className="col text-center"><FontAwesomeIcon icon={faHome} size="6x" /></div>
-            <div className="col text-center"><FontAwesomeIcon icon={faUser} size="3x" /></div>
-          </div>
-        </div>
-      </footer>
-      <style jsx>{`
-        
-        `}</style>
+    <style jsx>{`
+      .circle-user {
+        border: 1px solid blue;
+        border-radius: 50%;
+      }
+
+      .main-layout {
+        margin-top: 2vh;
+      }
+    `}</style>
     </div>
   )
 }
